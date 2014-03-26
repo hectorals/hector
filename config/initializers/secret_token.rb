3f6c9257355e94656f14c39735607f801c0b403f
Hector Selberis <selberis@gmail.com>
2014-03-26 12:01:13 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hector::Application.config.secret_key_base = '1e1425d0c2e2571890396ef948992d5746df6d62c63332c27022a673b68127efc201320d83d1623fe85c521947a4a1bc8c185acd116194301cab5451a4db01cf'
